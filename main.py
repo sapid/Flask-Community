b307b26d51d53007f459e4cc97b8bac1ec9fff62
Will Crawford <metawhimsy@gmail.com>
2013-03-02 02:04:20 -0800

    Adds session support

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from database import db_session
+from session import ItsdangerousSessionInterface
 import config
 __author__ = 'Will Crawford <will@metawhimsy.com>'
 
 app = Flask(__name__)
 app.config.from_object(config.DevelopmentConfig)
+app.session_interface = ItsdangerousSessionInterface()
 
 @app.teardown_request
 def shutdown_session(exception=None):
